abe54efafb1a53fb57257737eb5aa7c55f883813
A2PR <a2payema@gmail.com>
2018-04-08 13:42:53 -0400

    side drawer

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { NativeScriptHttpModule } from "nativescript-angular/http";
 import { AppRoutingModule } from "./app.routing";
+import {NativeScriptUISideDrawerModule} from 'nativescript-telerik-ui/sidedrawer/angular';
+
 import { AppComponent } from "./app.component";
 import { MenuComponent } from "./menu/menu.component";
 import { DishdetailComponent } from "./dishdetail/dishdetail.component";
+import { DrawerComponent } from "./shared/drawer/drawer.component";
 
 import { DishService } from "./services/dish.service";
 import { ProcessHTTPMsgService } from "./services/process-httpmsg.service";
 import{baseURL} from './shared/baseurl';
+
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
@@ -23,12 +27,14 @@ import{baseURL} from './shared/baseurl';
     imports: [
         NativeScriptModule,
         AppRoutingModule,
-        NativeScriptHttpModule
+        NativeScriptHttpModule,
+        NativeScriptUISideDrawerModule
     ],
     declarations: [
         AppComponent,
         MenuComponent,
-        DishdetailComponent
+        DishdetailComponent,
+        DrawerComponent
     ],
     providers: [
         DishService,
