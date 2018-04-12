6e0e4ee443b44344624cb2df83391bb33ca1e219
A2PR <a2payema@gmail.com>
2018-04-11 21:23:08 -0400

    advanced list

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,6 +4,7 @@ import { NativeScriptHttpModule } from "nativescript-angular/http";
 import { AppRoutingModule } from "./app.routing";
 import {NativeScriptUISideDrawerModule} from 'nativescript-telerik-ui/sidedrawer/angular';
 import {TNSFontIconModule} from 'nativescript-ngx-fonticon';
+import {NativeScriptUIListViewModule} from 'nativescript-telerik-ui/listview/angular';
 
 import { AppComponent } from "./app.component";
 import { MenuComponent } from "./menu/menu.component";
@@ -12,6 +13,7 @@ import { DrawerComponent } from "./shared/drawer/drawer.component";
 import { HomeComponent } from "./home/home.component";
 import { ContactComponent } from "./contact/contact.component";
 import { AboutComponent } from "./about/about.component";
+import { FavoritesComponent } from "./favorites/favorites.component";
 
 import { DishService } from "./services/dish.service";
 import { ProcessHTTPMsgService } from "./services/process-httpmsg.service";
@@ -19,6 +21,7 @@ import{baseURL} from './shared/baseurl';
 import { PromotionService } from "./services/promotion.service";
 import { LeaderService } from "./services/leader.service";
 import { FavoriteService } from "./services/favorite.service";
+
 
 
 
@@ -39,7 +42,8 @@ import { FavoriteService } from "./services/favorite.service";
         NativeScriptUISideDrawerModule,
         TNSFontIconModule.forRoot({
             'fa':'./fonts/font-awesome.min.css'}
-        )
+        ),
+        NativeScriptUIListViewModule
     ],
     declarations: [
         AppComponent,
@@ -48,7 +52,8 @@ import { FavoriteService } from "./services/favorite.service";
         DrawerComponent, 
         HomeComponent,
         ContactComponent,
-        AboutComponent
+        AboutComponent,
+        FavoritesComponent
     ],
     providers: [
         DishService,
