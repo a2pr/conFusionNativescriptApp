0ca1eed0bd8df4ca81bcd94fc0b2fd49e275211e
A2PR <a2payema@gmail.com>
2018-04-12 21:17:36 -0400

    forms

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from "./app.routing";
 import {NativeScriptUISideDrawerModule} from 'nativescript-telerik-ui/sidedrawer/angular';
 import {TNSFontIconModule} from 'nativescript-ngx-fonticon';
 import {NativeScriptUIListViewModule} from 'nativescript-telerik-ui/listview/angular';
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from "./app.component";
 import { MenuComponent } from "./menu/menu.component";
@@ -14,6 +16,7 @@ import { HomeComponent } from "./home/home.component";
 import { ContactComponent } from "./contact/contact.component";
 import { AboutComponent } from "./about/about.component";
 import { FavoritesComponent } from "./favorites/favorites.component";
+import { ReservationComponent } from "./reservation/reservation.component";
 
 import { DishService } from "./services/dish.service";
 import { ProcessHTTPMsgService } from "./services/process-httpmsg.service";
@@ -43,7 +46,9 @@ import { FavoriteService } from "./services/favorite.service";
         TNSFontIconModule.forRoot({
             'fa':'./fonts/font-awesome.min.css'}
         ),
-        NativeScriptUIListViewModule
+        NativeScriptUIListViewModule,
+        NativeScriptFormsModule,
+        ReactiveFormsModule
     ],
     declarations: [
         AppComponent,
@@ -53,7 +58,8 @@ import { FavoriteService } from "./services/favorite.service";
         HomeComponent,
         ContactComponent,
         AboutComponent,
-        FavoritesComponent
+        FavoritesComponent,
+        ReservationComponent
     ],
     providers: [
         DishService,
