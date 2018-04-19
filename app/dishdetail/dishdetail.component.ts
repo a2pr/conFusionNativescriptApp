8e1b4c497a6891270d25443cdf988666dda74412
A2PR <a2payema@gmail.com>
2018-04-18 20:26:06 -0400

    Animations and Gestures part 2

--- app/dishdetail/dishdetail.component.ts
+++ app/dishdetail/dishdetail.component.ts
@@ -11,6 +11,13 @@ import { FavoriteService } from '../services/favorite.service';
 import {confirm, action} from 'ui/dialogs';
 import { Toasty } from 'nativescript-toasty';
 import { CommentComponent } from '../Comment/comment.component';
+import { Page} from 'ui/page';
+import { Animation, AnimationDefinition } from 'ui/animation';
+import { SwipeDirection, SwipeGestureEventData } from 'ui/gestures';
+import {Color} from 'color';
+import * as enums from 'ui/enums';
+import {View} from 'ui/core/view';
+
 @Component({
     selector:'app-dishdetail',
     moduleId:module.id,
@@ -27,6 +34,12 @@ export class DishdetailComponent implements OnInit{
     avgstars:string;
     numcomments:number;
     favorite:boolean=false;
+
+    showComments:boolean=false;
+    cardImage:View;
+    commentList:View;
+    cardLayout:View;
+
     constructor( private dishservice:DishService,
         private favoriteService:FavoriteService,
         private route:ActivatedRoute,
@@ -34,6 +47,7 @@ export class DishdetailComponent implements OnInit{
         private routerExtensions: RouterExtensions,
         private modalService:ModalDialogService,
         private vcRef:ViewContainerRef,
+        private page:Page,
     @Inject('BaseURL' ) private BaseURL){   }
     ngOnInit(){
         this.route.params
@@ -98,5 +112,92 @@ export class DishdetailComponent implements OnInit{
     }
     goBack():void{
         this.routerExtensions.back();
+    }
+
+    onSwipe(args:SwipeGestureEventData){
+        if(args.direction===SwipeDirection.up){
+            this.animateUp();
+        }
+        else if(args.direction===SwipeDirection.down){
+            this.animateDown();
+        }
+    }
+
+    showAndHideComments(){
+        if(!this.showComments){
+            this.animateUp();
+        }
+        else{
+            this.animateDown();
+        }
+    }
+    animateUp(){
+        if(this.dish && !this.showComments){
+            this.cardImage=this.page.getViewById<View>('cardImage');
+            this.cardLayout=this.page.getViewById<View>('cardLayout');
+            this.commentList=this.page.getViewById<View>('commentList');
+
+            let definitions= new Array<AnimationDefinition>();
+            let a1:AnimationDefinition={
+                target:this.cardImage,
+                scale:{x:1,y:0},
+                translate:{x:0,y:-200},
+                opacity:0,
+                duration:500,
+                curve:enums.AnimationCurve.easeIn
+            };
+            definitions.push(a1);
+            let a2:AnimationDefinition={
+                target:this.cardLayout,
+                backgroundColor:new Color("#ffc107"),
+                duration:500,
+                curve:enums.AnimationCurve.easeIn
+            };
+            definitions.push(a2)
+            let animationSet=new Animation(definitions);
+            animationSet.play()
+                .then(()=>{
+                    this.showComments=true;
+                })
+                .catch((e)=>{
+                    console.log(e.message)
+                });
+        }
+    }
+
+    animateDown(){
+        if(this.dish && this.showComments){
+            this.cardImage=this.page.getViewById<View>('cardImage');
+            this.cardLayout=this.page.getViewById<View>('cardLayout');
+            this.commentList=this.page.getViewById<View>('commentList');
+
+            this.showComments=false;
+
+            let definitions= new Array<AnimationDefinition>();
+            let a1:AnimationDefinition={
+                target:this.cardImage,
+                scale:{x:1,y:1},
+                translate:{x:0,y:0},
+                opacity:1,
+                duration:500,
+                curve:enums.AnimationCurve.easeIn
+            };
+            definitions.push(a1);
+            let a2:AnimationDefinition={
+                target:this.cardLayout,
+                backgroundColor:new Color("#ffffff"),
+                duration:500,
+                curve:enums.AnimationCurve.easeIn
+            };
+            definitions.push(a2)
+            let animationSet=new Animation(definitions);
+            animationSet.play()
+                .then(()=>{
+
+                })
+                .catch((e)=>{
+                    console.log(e.message)
+                });
+        }
     }
 }
